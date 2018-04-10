143dfcff49a81c5e17b754bb44fce6289d1b550c
Rohith Gandhi G <36641932+grohith327@users.noreply.github.com>
2018-04-10 22:52:36 +0530

    Update Url_Retrieval.py

--- Url_Retrieval.py
+++ Url_Retrieval.py
@@ -1,7 +1,7 @@
 import bs4 as bs
 import urllib.request
 
-sauce = urllib.request.urlopen('https://en.wikipedia.org/wiki/Lionel_Messi').read()
+sauce = urllib.request.urlopen('URL').read()
 soup = bs.BeautifulSoup(sauce,'lxml')
 
 
